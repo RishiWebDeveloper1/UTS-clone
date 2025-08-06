1ec667e0a147e8ca348b69eb9161c0c02d50825b
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2025-08-07 00:38:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Fullscreen functionality
+function enterFullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { // Safari
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE11
+        elem.msRequestFullscreen();
+    }
+}
+
+// Enter fullscreen when page loads
+window.addEventListener('load', () => {
+    // Small delay to ensure page is fully loaded
+    setTimeout(() => {
+        enterFullscreen();
+    }, 100);
+});
+
+// Also try to enter fullscreen on first user interaction
+document.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+        enterFullscreen();
+    }
+}, { once: true });
+
 const imageBox = document.querySelector('.image-box');
 const mainImage = document.getElementById('mainImage');
 
